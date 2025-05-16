bec0426ad8bdabfc56026a048fd21755ece6ac56
ArchMagosF04 <francochialvo2004@gmail.com>
2025-05-16 16:45:30 -0300

    Weapon Knockback in progress

---
+++ Assets/_Scripts/Player/Weapons/Components/ComponentsData/AttackData/AttackKnockBack.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class AttackKnockBack : AttackData
+{
+    [field: SerializeField] public Vector2 Angle { get; private set; }
+}

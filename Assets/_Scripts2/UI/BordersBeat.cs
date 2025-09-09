942f92064a641db3f4d037c56a08ef7d16221c04
Reid3002 <reid3002@gmail.com>
2025-09-09 20:20:52 -0300

    Colored Borders

---
+++ Assets/_Scripts2/UI/BordersBeat.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BordersBeat : MonoBehaviour
+{
+    private bool OnBeat = true;
+    private Animator animator;
+    private Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+       image = GetComponent<Image>();
+        animator = GetComponent<Animator>();
+        BeatManager.Instance.intervals[2].OnBeatEvent += BeatEffectOn;
+        BeatManager.Instance.OnCorrectBeat += GoodHit;
+        BeatManager.Instance.OnWrongBeat += BadHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnBeat)
+        {
+            OnBeat = false;
+            animator.SetTrigger("Off");
+            
+        }
+    }
+
+    private void BeatEffectOn()
+    {
+        image.color = Color.white;
+        //gameObject.SetActive(true);
+        Debug.Log("Borders active");
+        animator.SetTrigger("On");
+        OnBeat = true;
+    }
+
+    private void GoodHit()
+    {
+        //image.color = Color.green;
+        animator.SetTrigger("Green");
+    }
+
+    private void BadHit()
+    {
+        animator.SetTrigger("Red");
+    }
+}

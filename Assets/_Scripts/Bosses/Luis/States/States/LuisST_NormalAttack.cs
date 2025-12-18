676ca535e23b46bec5cee80da54ed2bfd4481d12
ArchMagosF04 <francochialvo2004@gmail.com>
2025-12-18 13:36:54 -0300

    Level 3 Still Progressing

--- Assets/_Scripts/Bosses/Luis/States/States/LuisST_NormalAttack.cs
+++ Assets/_Scripts/Bosses/Luis/States/States/LuisST_NormalAttack.cs
@@ -1,10 +1,71 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LuisST_NormalAttack : LuisState
 {
+    private int beatTimer;
+
+    private bool attackPerformed;
+
     public LuisST_NormalAttack(LuisController controller, StateMachine stateMachine, LuisStats stats, Animator anim, string animBoolName) : base(controller, stateMachine, stats, anim, animBoolName)
     {
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+
+        beatTimer = 0;
+        attackPerformed = false;
+
+        BeatManager.Instance.intervals[0].OnBeatEvent += PerformAttack;
+    }
+
+    public override void UnsubscribeToEvents()
+    {
+        base.UnsubscribeToEvents();
+
+        BeatManager.Instance.intervals[0].OnBeatEvent -= PerformAttack;
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+        BeatManager.Instance.intervals[0].OnBeatEvent -= PerformAttack;
+        controller.DesiredAction = LuisController.ActionType.None;
+        anim.ResetTrigger("AttackBeat");
+    }
+
+    public void PerformAttack()
+    {
+        if (attackPerformed)
+        {
+            stateMachine.ChangeState(controller.IdleState);
+            return;
+        }
+
+        beatTimer++;
+
+        if (beatTimer == (1 + stats.BeatsBeforeNormalAttack))
+        {
+            controller.upBeam.SetAim();
+
+            if (controller.transform.position.x > 0) controller.leftBeam.SetAim();
+            else controller.rightBeam.SetAim();
+        }
+
+        if (beatTimer >= 2 + stats.BeatsBeforeNormalAttack)
+        {
+            anim.SetTrigger("AttackBeat");
+
+            controller.upBeam.FireBeam();
+
+            if (controller.transform.position.x > 0) controller.leftBeam.FireBeam();
+            else controller.rightBeam.FireBeam();
+
+            attackPerformed = true;
+        }
     }
 }

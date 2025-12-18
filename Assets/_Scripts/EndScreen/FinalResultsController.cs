c25c0c8dc4f593953d5926e92842d1bdc533509a
ArchMagosF04 <francochialvo2004@gmail.com>
2025-12-17 21:14:44 -0300

    EndingCutscene

--- Assets/_Scripts/EndScreen/FinalResultsController.cs
+++ Assets/_Scripts/EndScreen/FinalResultsController.cs
@@ -1,17 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class FinalResultsController : MonoBehaviour
+public class FinalResultsController : MonoBehaviour, IDataPersistance
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] endingTiers;
+
+    [SerializeField, TextArea] private string[] finalTextLines;
+
+    [SerializeField] private TMP_Text finalText;
+
+    private int starsUnloked;
+
+    private void Awake()
     {
-        
+        foreach (GameObject slider in endingTiers) slider.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        if (starsUnloked >= 10)
+        {
+            endingTiers[4].SetActive(true);
+            finalText.text = finalTextLines[4];
+        }
+        else if (starsUnloked >= 8)
+        {
+            endingTiers[3].SetActive(true);
+            finalText.text = finalTextLines[3];
+        }
+        else if (starsUnloked >= 6)
+        {
+            endingTiers[2].SetActive(true);
+            finalText.text = finalTextLines[2];
+        }
+        else if (starsUnloked >= 3)
+        {
+            endingTiers[1].SetActive(true);
+            finalText.text = finalTextLines[1];
+        }
+        else
+        {
+            endingTiers[0].SetActive(true);
+            finalText.text = finalTextLines[0];
+        }
+    }
+
+    public void LoadData(GameData gameData)
+    {
+        starsUnloked = gameData.GetAmountOfUnlockedStars();
+    }
+
+    public void SaveData(GameData gameData)
     {
         
     }
